947714c6c8c2fec3c98374d8f40d4944b4f09cdb
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2016-09-14 12:14:25 -0400

    Added language button.

--- js/rotate-glyphs.js
+++ js/rotate-glyphs.js
@@ -1,7 +1,17 @@
 window.onload = function () {
+	$('.fr').removeClass('hide-lang');
+	$('.en').addClass('hide-lang');
+}
+
+$("#bouton-anglais").click(function() {
 	$('.en').removeClass('hide-lang');
 	$('.fr').addClass('hide-lang');
-}
+});
+
+$("#bouton-francais").click(function() {
+	$('.fr').removeClass('hide-lang');
+	$('.en').addClass('hide-lang');
+});
 
 function rotateOnClick(glyph, name) {
 
